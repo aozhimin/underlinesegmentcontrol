3dcf541374bb5b2c275315cd82d66d8c0a648a52
dev-aozhimin <zhimin.ao@ele.me>
2015-09-16 16:43:31 +0800

    test gitignore

---
+++ underlinesegmentcontrol/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  underlinesegmentcontrol
+//
+//  Created by dev-aozhimin on 15/9/16.
+//  Copyright (c) 2015年 敖志敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ underlinesegmentcontrol/TestViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestViewController.m
+//  underlinesegmentcontrol
+//
+//  Created by dev-aozhimin on 15/9/16.
+//  Copyright (c) 2015年 敖志敏. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
